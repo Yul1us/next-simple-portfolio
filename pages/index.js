17fb2a1af8bede293853ab5ad521e291188540f8
Julio Aguero <TI@ELECTRODOMOTICA.COM.VE>
2026-01-07 22:30:17 -0400

    Refactor personal description in index.js

--- pages/index.js
+++ pages/index.js
@@ -19,11 +19,11 @@ const Index = () => (
                             <h1>Julio Aguero</h1>
                             <h3>FullStack Developer</h3>                            
                             <p>
-                                A lo largo de mas de 20 años he desempeñado varios cargos que tienen que ver con el desarrollo de software para las organizaciones empresariales para las que fui contratado, 
-                                A pesar de ser Ingeniero Electrónico en computación, lo que me apasiona es resolver problemas a través de aplicaciones de software. Comencé con lenguajes como VB 6.0, VB.Net, SQL y actualmente me encuentro en proyectos en la blockchain + WEB3 + WEB2 desarrollando aplicaciones con Solidity + JavaScript + C#, en 360°, Creando Tokens fungibles y no fungibles NFT en estandares ERC-20, ERC-721 entre otros, soluciones completas con Frontend modernas o en el Backend y API's. 
+                                A lo largo de más de 20 años he desarrollado aplicaciones de software para organizaciones empresariales, participando en todas las etapas hasta su implementación. 
+                                A pesar de ser ingeniero electrónico en computación, lo que me apasiona es resolver problemas a través de estas aplicaciones de software. En mis inicios comencé con lenguajes como VB 6.0, VB.Net, SQL y actualmente me encuentro en el desarrollo de proyectos Blockchain + WEB3 con lenguajes como Solidity + JavaScript y para + WEB2 con ASP .Net y C#, también creando Tokens fungibles FT y no fungibles NFT en estandares ERC-20, ERC-721 entre otros, soluciones integrales en 360°, completas tanto en Frontend modernas o en Backend con API's. 
                                 Con esto estoy pasando de entornos Desktops a entornos multiplataforma y en la web2 y ahora con Smart Contract (DEFI), Tokenización a la Web3 en bases de datos descentralizadas. También soy administrador de bases de datos MS SQL con una amplia experticia en otros motores de bases de datos.
-                                Dentro de las aplicaciones más relevantes que he desarrollado están sistemas: administrativos, financieros, contables, crediticios, nóminas, sistemas de pesaje con balanzas camioneras, sistemas para el sector de la medicina, entre otros...
-								En fin, el plan actual es conquistar la WEB3 en el mundo Blockchain, bajo el nombre del proyecto SIMON 360 el cual es un ERP en el cual le he dedicado a crecer durante los últimos años.
+                                Dentro de los sectores más relevantes en los que he desarrollado aplicaciones de software están los sistemas: administrativos, financieros, contables, crediticios, nóminas, sistemas de pesaje con balanzas camioneras, sistemas para el sector de la medicina, entre otros...
+								En fin, el plan actual es conquistar la WEB3 en el mundo Blockchain, bajo el nombre del proyecto SIMON 360 el cual es un ERP con más de 100 módulos funcionales y que sigue mejorando día a día durante los últimos años.
                             </p>
                             <a href='/hireme'>Hire me</a>
                         </div>
